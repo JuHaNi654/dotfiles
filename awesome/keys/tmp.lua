e2fbe84f5d6589d51510e25909209b853df6798a
JuHaNi654 <JuHaNi654@users.noreply.github.com>
2025-01-18 13:31:00 +0200

    Removed duplicate file

--- awesome/keys/tmp.lua
+++
@@ -1,9 +0,0 @@
-local terminal = "x-terminal-emulator"
-local editor = os.getenv("EDITOR") or "editor"
-local editor_cmd = terminal .. " -e " .. editor
-
-return {
-	terminal = terminal,
-	editor = editor,
-	editor_cmd = editor_cmd,
-}
