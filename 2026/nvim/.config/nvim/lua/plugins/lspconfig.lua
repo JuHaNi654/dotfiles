6329091d70c3c168c5e8b1274dee8429bd2ce6f5
JuHaNi654 <JuHaNi654@users.noreply.github.com>
2026-07-20 14:32:31 +0300

    Added vim-fugitive plugin

--- 2026/nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ 2026/nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -1,5 +1,17 @@
 return {
   "neovim/nvim-lspconfig",
+  init = function()
+    vim.api.nvim_create_autocmd("LspAttach", {
+      callback = function(args)
+        local bufname = vim.api.nvim_buf_get_name(args.buf)
+        if bufname:match("^fugitive://") or bufname:match("^gitsigns://") then
+          vim.schedule(function()
+            vim.lsp.buf_detach_client(args.buf, args.data.client_id)
+          end)
+        end
+      end,
+    })
+  end,
   ---@class PluginLspOpts
   opts = {
     ---@type lspconfig.options
